13f36de921492668e8727b7f92ee0cafaabc931a
Malemngalba <lkonsam@gmail.com>
2024-12-26 12:46:50 +0530

    Test Commit

--- qtify/src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Button.module.css";
-
-
-export default  function Button({ children }) {
-    return <Button class={styles.button} >{children}</Button> ;
-}
-  
