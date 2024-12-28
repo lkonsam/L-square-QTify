5b18a778cc82b172c91ad8ce9b78a0e57704a253
lkonsam <lkonsam@gmail.com>
2024-12-28 11:49:06 +0530

    first submit

--- qtify/src/components/Home/Home.jsx
+++ qtify/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Hero from "../Hero/Hero";
 import Navbar from "../Navbar/Navbar";
 
 export default function Home({ props }) {
   return (
     <>
-      <Navbar searchData="Hello" />
+      <Navbar searchData="" />
+      <Hero />
     </>
   );
 }
